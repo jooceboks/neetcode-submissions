983f14b63ea7ec9d43f51e1ba65ef035739ea9ae
Sarah Liu <120229681+jooceboks@users.noreply.github.com>
2026-07-21 14:26:55 -0500

    Add: is-palindrome - submission-10

---
+++ Data Structures & Algorithms/is-palindrome/submission-10.py
@@ -0,0 +1,8 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        string = ""
+        for char in s:
+            if char.isalnum():
+                string += char.lower()
+        return True if string == string[::-1] else False
+
